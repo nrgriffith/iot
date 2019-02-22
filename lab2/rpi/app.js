4e9bde629855015a25611ba8d3951f9a52a56476
Sam Morgan <sam.morgan44@gmail.com>
2019-02-22 01:33:36 +0000

    cleaned up code and added comments added screenshots and zip file containing code

--- lab2/rpi/app.js
+++ lab2/rpi/app.js
@@ -4,6 +4,8 @@ var nodeimu = require('nodeimu');
 var IMU = new nodeimu.IMU();
 var sense = require('sense-hat-led');
 
+//config values were obtained directly from the website that stores our database
+//https://console.firebase.google.com/project/iot-lab2-c1bfa/overview
 var config = {
     apiKey: "AIzaSyDeXPowwDP1M02vsDZkkeLDMfU1_t6SrEs", 	//apikey
     authDomain: "iot-lab2-c1bfa.firebaseapp.com",	//id
@@ -18,12 +20,17 @@ firebase.initializeApp(config);
 // Get a reference to the database service
 var database = firebase.database();
 
+//setInterval the function getSensorData is called (in milliseconds)
 setInterval(getSensorData, 60000);
+//set updateLight function to be called everytime the value of update_lght is changed
 database.ref().child('update_light').on('value', function(snapshot){updateLight(snapshot);});
 
+//global variable used to update database
 var updates = {};
+//clear lights on rasberry pi
+sense.clear();
 
-clearLights();
+console.log("\n");
 
 function getSensorData(){
     
@@ -31,15 +38,15 @@ function getSensorData(){
     var data = IMU.getValueSync();
 
     //get temperature
-    var temperature = data.temperature.toFixed(4);
+    var temperature = data.temperature;
     //console.log(temperature); 
 
-    //get pressure
-    var pressure = data.pressure.toFixed(4);
+    //get pressure (thought we needed this, but we didn't)
+    //var pressure = data.pressure;
     //console.log(pressure);
 
     //get humidity
-    var humidity = data.humidity.toFixed(4);
+    var humidity = data.humidity;
     //console.log(humidity);   
 	
 	console.log("data gathered, uploading to database...");
@@ -48,39 +55,41 @@ function getSensorData(){
 
 
 function uploadSensorData(temperature, humidity){
+	//edit updates variable to read in values
 	updates['/temperature'] = temperature;
 	updates['/humidity'] = humidity;	
 	
+	//update database
     database.ref().update(updates);
 
-	console.log("data uploaded successfully...\n");
+	console.log("data uploaded successfully\n");
 }
 
+//function is called when update_light changes
 function updateLight(snapshot){
 	if(snapshot.val() == true){
-		console.log("Changing light color...");
-		var data = database.ref().child('/').once('value', 
+		//.once reads the value one time instead of everytime the value is changed
+		database.ref().child('/').once('value', 
 			function(data){
+				console.log("Changing light in row: %s col: %s...", data.val().light_row, data.val().light_column);
+				
+				//set light to value reflected in the database
 				sense.setPixel(
-				data.val().light_row,
 				data.val().light_column,
+				data.val().light_row,
 				data.val().light_r,
 				data.val().light_g,
 				data.val().light_b)
+
+				console.log("light set\n");
+
+				//this code would set update_light to false again which was used when testing
+				//console.log("setting update_light to false...");
+				//updates['/update_light'] = false;
+				//database.ref().update(updates);
+				//console.log("done\n");
 			}
 		);
-		console.log("light set...");
-		console.log("setting update_light to false...\n");
-		updates['/update_light'] = false;
-		database.ref().update(updates);
 	}
 }
 
-function clearLights(){
-	console.log("clearing all lights...");
-	for(var i=0; i<8; i++){
-		for(var j=0; j<8; j++){
-			sense.setPixel(i,j,[0,0,0]);
-		}
-	}
-}
