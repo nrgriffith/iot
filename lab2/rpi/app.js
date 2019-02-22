c47b1b37f9daed734a588d16ed75eb5736373f97
Sam Morgan <sam.morgan44@gmail.com>
2019-02-22 00:37:58 +0000

    program now reads and uploads temp and humidity data once a minute if database value update_light is set to true program will set the appropiate light no the rasberry pi

--- lab2/rpi/app.js
+++ lab2/rpi/app.js
@@ -4,9 +4,6 @@ var nodeimu = require('nodeimu');
 var IMU = new nodeimu.IMU();
 var sense = require('sense-hat-led');
 
-console.log("setting color to blue...");
-sense.setPixel(0,0,[0,0,254]);
-
 var config = {
     apiKey: "AIzaSyDeXPowwDP1M02vsDZkkeLDMfU1_t6SrEs", 	//apikey
     authDomain: "iot-lab2-c1bfa.firebaseapp.com",	//id
@@ -21,5 +18,69 @@ firebase.initializeApp(config);
 // Get a reference to the database service
 var database = firebase.database();
 
-console.log("terminating...");
-sense.setPixel(0,0,[0,0,0]);
+setInterval(getSensorData, 60000);
+database.ref().child('update_light').on('value', function(snapshot){updateLight(snapshot);});
+
+var updates = {};
+
+clearLights();
+
+function getSensorData(){
+    
+    //get all sensor data
+    var data = IMU.getValueSync();
+
+    //get temperature
+    var temperature = data.temperature.toFixed(4);
+    //console.log(temperature); 
+
+    //get pressure
+    var pressure = data.pressure.toFixed(4);
+    //console.log(pressure);
+
+    //get humidity
+    var humidity = data.humidity.toFixed(4);
+    //console.log(humidity);   
+	
+	console.log("data gathered, uploading to database...");
+	uploadSensorData(temperature, humidity);
+}
+
+
+function uploadSensorData(temperature, humidity){
+	updates['/temperature'] = temperature;
+	updates['/humidity'] = humidity;	
+	
+    database.ref().update(updates);
+
+	console.log("data uploaded successfully...\n");
+}
+
+function updateLight(snapshot){
+	if(snapshot.val() == true){
+		console.log("Changing light color...");
+		var data = database.ref().child('/').once('value', 
+			function(data){
+				sense.setPixel(
+				data.val().light_row,
+				data.val().light_column,
+				data.val().light_r,
+				data.val().light_g,
+				data.val().light_b)
+			}
+		);
+		console.log("light set...");
+		console.log("setting update_light to false...\n");
+		updates['/update_light'] = false;
+		database.ref().update(updates);
+	}
+}
+
+function clearLights(){
+	console.log("clearing all lights...");
+	for(var i=0; i<8; i++){
+		for(var j=0; j<8; j++){
+			sense.setPixel(i,j,[0,0,0]);
+		}
+	}
+}
